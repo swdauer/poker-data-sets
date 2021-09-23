75a777bbfd7d704c6cdf55961341cb4a2fbf9591
Scott Dauer <swdauer@gmail.com>
2021-09-22 20:16:40 -0500

    Add TODO Add more manipulation functions Add data definitions in data.R

---
+++ data.R
@@ -0,0 +1,38 @@
+ranks <- c("2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A")
+
+handRanks <- unlist(lapply(ranks, 
+                           function(y) lapply(ranks[match(y,ranks):length(ranks)],
+                                              function(x) c(y, x))),
+                    recursive = FALSE)
+
+allHands <- unlist(lapply(handRanks, function(x)
+  if (x[1] == x[2]) {
+    list(c(x[1], x[2], FALSE))
+  } else {
+    list(
+      c(x[1], x[2], FALSE),
+      c(x[1], x[2], TRUE)
+    )
+  }), recursive = FALSE)
+
+firstSet <- read.csv("texasHoldEmHeadsUp_01_1000000.csv")
+secondSet <- read.csv("texasHoldEmHeadsUp_02_1000000.csv")
+thirdSet <- read.csv("texasHoldEmHeadsUp_03_1000000.csv")
+fourthSet <- read.csv("texasHoldEmHeadsUp_04_1000000.csv")
+fifthSet <- read.csv("texasHoldEmHeadsUp_05_1000000.csv")
+sixthSet <- read.csv("texasHoldEmHeadsUp_06_1000000.csv")
+seventhSet <- read.csv("texasHoldEmHeadsUp_07_1000000.csv")
+eighthSet <- read.csv("texasHoldEmHeadsUp_08_1000000.csv")
+ninthSet <- read.csv("texasHoldEmHeadsUp_09_1000000.csv")
+tenthSet <- read.csv("texasHoldEmHeadsUp_10_1000000.csv")
+
+totalSet <- rbind(firstSet,
+                  secondSet,
+                  thirdSet,
+                  fourthSet,
+                  fifthSet,
+                  sixthSet,
+                  seventhSet,
+                  eighthSet,
+                  ninthSet,
+                  tenthSet)
--- manipulationFunctions.R
+++ manipulationFunctions.R
@@ -14,16 +14,12 @@ suited <- function(lowCard, highCard) {
   getSuit(lowCard) == getSuit(highCard)
 }
 
-ranks <- c("A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2")
-
-possibleHandRanks <- lapply(ranks, function(y) lapply(ranks[match(y,ranks):length(ranks)],function(x) c(y, x)))
-
-winRateHand1 <- function(set, lowRank, highRank, suited) {
-  if (suited) {
-    nrow(set[hasRanks(set$hand1_0, set$hand1_1, lowRank, highRank) & suited(set$hand1_0, set$hand1_1) & set$winner == 1,]) /
-      nrow(set[hasRanks(set$hand1_0, set$hand1_1, lowRank, highRank) & suited(set$hand1_0, set$hand1_1),])
-  } else {
-    nrow(set[hasRanks(set$hand1_0, set$hand1_1, lowRank, highRank) & !suited(set$hand1_0, set$hand1_1) & set$winner == 1,]) /
-      nrow(set[hasRanks(set$hand1_0, set$hand1_1, lowRank, highRank) & !suited(set$hand1_0, set$hand1_1),])
-  }
+occurHandI <- function(set, handNum, lowRank, highRank, suited) {
+  card1 = paste0("hand", toString(handNum), "_0")
+  card2 = paste0("hand", toString(handNum), "_1")
+  set[hasRanks(set[[card1]],
+               set[[card2]],
+               lowRank,
+               highRank)
+      & !xor(suited,suited(set[[card1]], set[[card2]])),]
 }
